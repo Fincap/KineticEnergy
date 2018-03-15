2ba924dbc95fb962f96064c81b0dfac90d484af7
Fincap <finnkerman@gmail.com>
2018-03-15 20:27:52 +1100

    Fixed integer division error

--- src/KineticEnergy.java
+++ src/KineticEnergy.java
@@ -9,20 +9,19 @@ Assignment number:	1
 import java.util.Scanner;
 
 public class KineticEnergy {
-	
+
 	public static void main(String[] args) {
 		//Variable declaration
 		double ballRadius, ballLinearVelocity;
-		double angularVelocity, momentOfInertia, mVariable; //I have no idea what m in the assignment is supposed to represent
+		double angularVelocity, momentOfInertia, ballMass; //I have no idea what m in the assignment is supposed to represent
 		double totalKineticEnergy, linearKineticEnergy, angularKineticEnergy;
-		
+
 		final double IRON_DENSITY = 7.8;
-		final double PI = 3.14;
-		
+
 		Scanner input = new Scanner(System.in);
-		
+
 		//If the radius and velocity were not command-line arguments, get them from user now
-		if (args.length == 2 ) {
+		if (args.length == 2) {
 			ballRadius = Double.parseDouble(args[0]);
 			System.out.printf("Ball radius loaded as: %.2f\n", ballRadius);
 			ballLinearVelocity = Double.parseDouble(args[1]);
@@ -33,34 +32,24 @@ public class KineticEnergy {
 			System.out.print("Enter ball linear velocity: ");
 			ballLinearVelocity = input.nextDouble();
 		}
-		
-		//TODO calculates wrong velocity. Manual calci
-		
+
 		//Calculate angular velocity
 		angularVelocity = ballLinearVelocity / ballRadius;
-		
-		System.out.println(angularVelocity);
-		
+
 		//Calculate moment of inertia
-		//TODO given 1, 1 as v and r, produces 24.492 instead of 32.656 Possibly has something to do with a different order of operations
-		mVariable = (4/3) * PI * Math.pow(ballRadius, 3) * IRON_DENSITY;
-		System.out.println(mVariable);
-		momentOfInertia = 0.4 * mVariable * Math.pow(ballRadius, 2);
-		System.out.println(momentOfInertia);
-		
+		ballMass = (4.0 / 3.0) * Math.PI * Math.pow(ballRadius, 3) * IRON_DENSITY;
+		momentOfInertia = 0.4 * ballMass * Math.pow(ballRadius, 2);
+
 		//Calculate angular kinetic energy
 		angularKineticEnergy = 0.5 * momentOfInertia * Math.pow(angularVelocity, 2);
-		System.out.println(angularKineticEnergy);
-		
+
 		//Calculate linear kinetic energy
-		linearKineticEnergy = 0.5 * mVariable * Math.pow(ballLinearVelocity, 2);
-		System.out.println(linearKineticEnergy);
-		
+		linearKineticEnergy = 0.5 * ballMass * Math.pow(ballLinearVelocity, 2);
+
 		//Calculate total kinetic energy
-		totalKineticEnergy = linearKineticEnergy + angularKineticEnergy	;
-		
+		totalKineticEnergy = linearKineticEnergy + angularKineticEnergy;
+
 		System.out.printf("The total kinetic energy is: %.2f\n", totalKineticEnergy);
-		
+
 	}
-	
 }
